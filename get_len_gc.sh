674bb8bfc97d95ee74bd7f392bbe98e2d880e5b3
ahmedtariq <a.t.o@outlook.com>
2021-02-16 09:32:41 +0200

    Initial commit with a shell script that calculates the gc content as well as the length of the specified feature throw: 1- detect and read the the gtf and fa files at each species data/raw 2- if no detected gtf or fa files in user specified dir, exit with error msg 3- create stage and output directories 4- filter the user specified feature into stage/[species]/gtfFileName.[feture].gtf 5- if the feature is exon, dropping duplicate using the field 18 (exon_id) is made before writing the filtered file 6- print msg with filtered count 7- raise error msg if there is no records remainig 8- calculate the gc content using bed tools into stage/[species]/gtfFileName.[feture].stats.gtf 9- raise msg with record count in stats 10- calculate the avg gc content and length of feature from stats into output/[species]/gtfFileName.[feature].stats.csv

---
+++ get_len_gc.sh
@@ -0,0 +1,69 @@
+#!bin/bash
+
+dir=$1
+fet=$2
+#  getting the fa and gtf files
+sp_fa=`find $dir -name '*.fa' -type f`
+sp_gtf=`find $dir -name '*.gtf' -type f`
+
+if [  -z "${sp_fa}" ] || [ -z "${sp_gtf}" ]
+then
+	echo "no gtf or fa files in ${dir}"
+else
+	## Initial Preparation
+	# creating stage dir with species under it
+	stg_dir=${dir/"raw"/"stage"}
+	mkdir -p $stg_dir
+	# creating output dir with species under it
+	out_dir=${dir/"raw"/"output"}
+	mkdir -p $out_dir
+
+	# extracting species name
+	sp="${sp_gtf##*/}"
+	sp="${sp%%_*}"
+
+	## filtering feature from gtf
+	filt_gtf_name="${sp_gtf##*/}"
+	filt_gtf_name=${filt_gtf_name/".gtf"/".${fet}.gtf"}
+	filt_gtf_path="${stg_dir}${filt_gtf_name}"
+
+	# removing all chromosomes starting with mM (Mitochondria) and filtering out feature
+	# in case of choosing exon as feature, duplicate exon records are removed
+	if [ $fet != "exon" ]
+	then
+		awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf > $filt_gtf_path
+	else
+		awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf | sort -u -t' ' -k18,18 > $filt_gtf_path
+	fi
+	
+	lc=`cat $filt_gtf_path | wc -l`
+	RED='\033[0;31m'
+	GREEN='\e[92m'
+	NC='\033[0m'
+	
+        # checking if filter output contains 0 records and removing if so
+	if [ $lc -eq 0 ]
+        then
+                echo -e "${RED} your input feature (${fet}) doesnot exist in ${sp} gtf file ${NC}"
+		rm $filt_gtf_path
+		exit 128
+	else
+		echo -e "${GREEN} filtered record for ${fet} in ${filt_gtf_name} is ${lc} ${NC}"
+	fi
+
+	## calculate the len and gc content per feature
+	stats_gtf_name=${filt_gtf_name/".gtf"/".stats.gtf"}
+	stats_gtf_path="${stg_dir}${stats_gtf_name}"
+
+	bedtools nuc -fi $sp_fa -bed $filt_gtf_path > $stats_gtf_path
+	lc=`cat $stats_gtf_path | wc -l`
+	echo -e "${GREEN} filtered record for ${fet} in ${stats_gtf_name} is ${lc} ${NC}"
+	
+	# Aggregate the avg GC and len
+	agg_csv_name=${stats_gtf_name/".gtf"/".csv"}
+	agg_csv_path="${out_dir}${agg_csv_name}"
+
+	awk -v sp="${sp}" -v fet="${fet}" 'BEGIN{FS="\t"; OFS=","; print "species,"fet"_avg_gc,"fet"_avg_len"} { total_gc += $11; count_gc++; total_len +=$18; count_len++ } \
+	END { print sp,total_gc/count_gc,total_len/count_len }' $stats_gtf_path > $agg_csv_path
+
+fi
