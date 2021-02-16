b45e147e47e6564cccc4eda20dc88a9ff038f793
ahmedtariq <a.t.o@outlook.com>
2021-02-16 09:56:00 +0200

    1- A new functionality of accepting a filter after featre from user for any tag value pair in gtf column 8 is added now you can make ./get_len_gc.sh data/raw/Homo_Sapien exon transcriptome_biotype=protein_coding 2- now accepting a 3rd argument in script in form of tag=value and parsing it to be used for filteration 2- enhanced styles for the error msgs and

--- get_len_gc.sh
+++ get_len_gc.sh
@@ -1,7 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
 dir=$1
 fet=$2
+tagvalue=$3
+tag="${tagvalue%%=*}"
+val="${tagvalue##*=}"
 #  getting the fa and gtf files
 sp_fa=`find $dir -name '*.fa' -type f`
 sp_gtf=`find $dir -name '*.gtf' -type f`
@@ -24,18 +27,34 @@ else
 
 	## filtering feature from gtf
 	filt_gtf_name="${sp_gtf##*/}"
-	filt_gtf_name=${filt_gtf_name/".gtf"/".${fet}.gtf"}
+	filt_gtf_name=${filt_gtf_name/".gtf"/".${fet}${tag}${val}.gtf"}
 	filt_gtf_path="${stg_dir}${filt_gtf_name}"
 
 	# removing all chromosomes starting with mM (Mitochondria) and filtering out feature
 	# in case of choosing exon as feature, duplicate exon records are removed
-	if [ $fet != "exon" ]
+	if [ -z "${tagvalue}" ]
 	then
+		if [ $fet != "exon" ]
+		then
 		awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf > $filt_gtf_path
-	else
+		else
 		awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf | sort -u -t' ' -k18,18 > $filt_gtf_path
+		fi
+	else
+        	if [ $fet != "exon" ]
+        	then
+        	awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf | \
+		awk -v tag="${tag}" -v val="${val}" '{for(i=1; i<=NF; i++) {if($i==tag) {tag_found=1} else if(tag_found==1 && index($i,val) != 0) {tag_found=0; print $0} else {tag_found=0}}}' > $filt_gtf_path
+        	else
+        	awk -v fet="${fet}" '$1 ~ /^[^Mm#]/ {if ($3 == fet) print}' $sp_gtf | \
+		awk -v tag="${tag}" -v val="${val}" '{for(i=1; i<=NF; i++) {if($i==tag) {tag_found=1} else if(tag_found==1 && index($i,val) != 0) {tag_found=0; print $0} else {tag_found=0}}}' | \
+		sort -u -t' ' -k18,18 > $filt_gtf_path
+        	fi
 	fi
-	
+
+
+
+
 	lc=`cat $filt_gtf_path | wc -l`
 	RED='\033[0;31m'
 	GREEN='\e[92m'
@@ -44,11 +63,11 @@ else
         # checking if filter output contains 0 records and removing if so
 	if [ $lc -eq 0 ]
         then
-                echo -e "${RED} your input feature (${fet}) doesnot exist in ${sp} gtf file ${NC}"
+                echo -e "${RED} your input feature (${fet} ${tagvalue}) doesnot exist in ${sp} gtf file ${NC}"
 		rm $filt_gtf_path
 		exit 128
 	else
-		echo -e "${GREEN} filtered record for ${fet} in ${filt_gtf_name} is ${lc} ${NC}"
+		echo -e "${GREEN} filtered record for ${fet} ${tagvalue} in ${filt_gtf_name} is ${lc} ${NC}"
 	fi
 
 	## calculate the len and gc content per feature
@@ -63,7 +82,7 @@ else
 	agg_csv_name=${stats_gtf_name/".gtf"/".csv"}
 	agg_csv_path="${out_dir}${agg_csv_name}"
 
-	awk -v sp="${sp}" -v fet="${fet}" 'BEGIN{FS="\t"; OFS=","; print "species,"fet"_avg_gc,"fet"_avg_len"} { total_gc += $11; count_gc++; total_len +=$18; count_len++ } \
+	awk -v sp="${sp}" -v fet="${fet}_${tagvalue}" 'BEGIN{FS="\t"; OFS=","; print "species,"fet"_avg_gc,"fet"_avg_len"} { total_gc += $11; count_gc++; total_len +=$18; count_len++ } \
 	END { print sp,total_gc/count_gc,total_len/count_len }' $stats_gtf_path > $agg_csv_path
 
 fi
