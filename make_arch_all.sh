bee56d46f448c1dfcbe1e4b928892661501ec03a
ahmedtariq <a.t.o@outlook.com>
2021-02-18 01:45:16 +0200

    Updating the make_arch_all.sh to allow the user to chose the data dir and easily make feature using abstract names script accepts 2 or more args from user, the 1st is data dir and 2nd is 1 or more feature seprated by space 1- data dir is parsed then teh list of feature is checked for > 1 input and that all features added are allowed 2- any duplicate features are removed 3- looping over features and mapping the naming to corespondand get_len_gc.sh args 4- applting the get_len_gc.sh script using the mapped args

--- make_arch_all.sh
+++ make_arch_all.sh
@@ -1,61 +1,89 @@
 #!/bin/bash
 
+# This script generalize applying stats like (avg gc content, avg length, count ) for all species under user input containing dir given that each species dir contains gtf and fasta file for species genome
+# There are minimum 2 mandatory arguments :
+# arg1: the path to directory containing all the species directories containing gtf and fasta files (uncompressed). Dir must be named raw e.g (data/raw/)
+# arg2,3,4,...: 1 to 3 characters representing the features that will be filtered for stats. with the following naming:
+#	filteration criteria: can be:
+#				p for pretein coding or
+#				np for non proteing coding
+#	feature type: can be:
+#			     g for gene
+#			     e for exon
+#			     f for five_prime_utr
+#			     t for three_prime_utr
+#	e.g pg for protein coding gene; npt for non protein coding three_prime_utr
+# example: to calculate the stats of (non protein coding genes, protein coding exons and all five_prime_utr) for all species under data/raw/ use the following
+# ./make_arch_all.sh data/raw/ npg pe f
 
-echo "Make gene arch 1/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ gene; done
 
-sleep 0.5
 
-echo "Make protein coding gene arch 2/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ gene -i gene_biotype=protein_coding ; done
+# setting input vars
+sp_dir=$1
+shift
+fets=("$@")
 
-sleep 0.5
 
-echo "Make non-protein coding gene arch 3/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ gene -e gene_biotype=protein_coding ; done
+# setting counter vars
+x=1
 
-sleep 0.5
+# setting color vars
+RED='\033[0;31m'
+GREEN='\e[92m'
+NC='\033[0m'
 
-echo "Make exon arch 4/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ exon ; done
+# drop duplicte
+fets=$(echo "${fets[@]}" | tr ' ' '\n' | sort -u)
+fets=($fets)
+num_fets=${#fets[@]}
+# check at least 1 feture arg
+if [ $num_fets -eq 0 ]; then
+	echo -e "${RED} at least 1 feature arg must be provided ${NC}"
+	exit 128
+fi
+# check input feature args
+for fet in "${fets[@]}"; do
+	if [[ "${fet: -1}" != *g* && "${fet: -1}" != *e* && "${fet: -1}"  != *t* && $fet != *f* ]]; then
+		echo -e "${RED} last character in all args must be g (gene), e (exon), t (three_prime_utr) or f (five_prime_utr) ${NC}"
+		exit 128
+	fi
+	if [[ "${fet::-1}" != *np* && "${fet::-1}" != *p* && "${fet::-1}" != "" ]]; then
+		echo -e "${RED} second last 2 characters in all args must be p (protein coding), np (non-protein coding) or empty ${NC}"
+		exit 128
+	fi
+done
 
-sleep 0.5
 
-echo "Make protein coding exon arch 5/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ exon -i transcript_biotype=protein_coding ; done
+for fet in "${fets[@]}"; do
 
-sleep 0.5
+	fet=${fet,,}
 
-echo "Make non-protein coding exon arch 6/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ exon -e transcript_biotype=protein_coding ; done
+	if [[ "${fet}" =~ "g" ]]; then
+		fet_arg="gene"
+		filt="gene_biotype=protein_coding"
+	elif [[ "${fet}" =~ "e" ]]; then
+		fet_arg="exon"
+		filt="transcript_biotype=protein_coding"
+	elif [[ "${fet}" =~ "f" ]]; then
+		fet_arg="five_prime_utr"
+		filt="transcript_biotype=protein_coding"
+	elif [[ "${fet}" =~ "t" ]]; then
+		fet_arg="three_prime_utr"
+		filt="transcript_biotype=protein_coding"
+	fi
 
-sleep 0.5
+	if [[ "${fet}" =~ "p" ]] && [[ "${fet}" != *"n"*  ]]; then
+		ude="-i"
+	elif [[ "${fet}" =~ "p" ]] && [[ "${fet}" =~ "n"  ]]; then
+		ude="-e"
+	else
+		ude=""
+		filt=""
+	fi
 
-echo "Make five_prime_utr arch 7/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ five_prime_utr ; done
+	echo "Make ${fet_arg} ${ude} ${filt} arch ${x}/${num_fets}"
 
-sleep 0.5
+	for i in `ls $sp_dir`; do echo $i; ./get_len_gc.sh $sp_dir/$i/ $fet_arg $ude $filt ; done
 
-echo "Make protein coding five_prime_utr arch 8/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ five_prime_utr -i transcript_biotype=protein_coding ; done
-
-sleep 0.5
-
-echo "Make non-protein coding five_prime_utr arch 9/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ five_prime_utr -e transcript_biotype=protein_coding ; done
-
-sleep 0.5
-
-echo "Make three_prime_utr arch 10/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ three_prime_utr ; done
-
-sleep 0.5
-
-echo "Make protein coding three_prime_utr arch 11/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ three_prime_utr -i transcript_biotype=protein_coding ; done
-
-sleep 0.5
-
-echo "Make non-protein coding three_prime_utr arch 12/12"
-for i in `ls data/raw/`; do echo $i; ./get_len_gc.sh data/raw/$i/ three_prime_utr -e transcript_biotype=protein_coding ; done
-
+	((x++))
+done
