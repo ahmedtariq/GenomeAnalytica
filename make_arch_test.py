0f4b9da4984173a21adc26821805a9999e64e94a
ahmedtariq <a.t.o@outlook.com>
2021-02-21 19:11:38 +0200

    adding a boxplot feature

--- make_arch_test.py
+++ make_arch_test.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import mannwhitneyu, ttest_ind
 from statsmodels.stats.multitest import multipletests
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import sys
 import argparse
@@ -15,6 +17,10 @@ import random
 import os
 from functools import reduce
 
+sns.set(rc={'figure.figsize':(25,20)})
+plt.rcParams.update({'font.size': 22})
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
 pd.set_option('display.max_columns',None)
 pd.set_option('display.max_colwidth', None)
 
@@ -26,8 +32,9 @@ desc = """make_arch_test
 This script takes the path for data created by make_arch_all, path for species lineage csv created by make_tax, number for tested rank and rank value to test againist. 
 Count the nulls (which is filled with 0 and returned as 0_count) in each feature, applies 2 sample 2 tailed ttest and Manwhetney, correct using benferroni and returns
 parsed data architicture feature in 1 csv
-test results containing (feature,0_count,avg_[rank_value],avg_non_[rank_value],MannWhitney_p,ttest_p,MannWhitney_adj_p,ttest_adj_p,MannWhitney_adj_reject,ttest_adj_reject)
+test results containing (feature,0_count,MannWhitney_p,ttest_p,MannWhitney_adj_p,ttest_adj_p,MannWhitney_adj_reject,ttest_adj_reject)
 filtered parsed data architicture features for only significant ones according to corrected p-value < 0.05
+Box plot figure for adj_ttest significant features
 """
 use = "./make_arch_test.py --stats input_stats_dir --tax tax_file --tax_rank number --rank_value rank_value [--out_test output_test_file] [--out_parsed output_parsed_file] [--out_parsed_filtered output_filtered_file]"
 
@@ -45,6 +52,7 @@ parser.add_argument('--tax', nargs='?',type=str, help='File csv contianing the f
 parser.add_argument('--out_test', nargs='?',type=str, default='test_result.csv', help='File to write the test results')
 parser.add_argument('--out_parsed', nargs='?',type=str, default='parsed_all.csv', help='File to write parsed data')
 parser.add_argument('--out_parsed_filtered', nargs='?',type=str, default='parsed_filtered.csv', help='File to write parsed data with filtered significant features')
+parser.add_argument('--out_fig', nargs='?',type=str, default='Sig_Fet_boxplot.png', help='png File to write boxplot figure')
 parser.add_argument('--tax_rank', nargs='?',type=int, default=10, help='the rank of lineage to be used e.g: 10 to use the supphylum')
 parser.add_argument('--rank_value', nargs='?',type=str, default="Vertebrata", help='value to test for in the tax rank e.g: Vertebrata to test Vert vs In-Vert in Subphylum')
 
@@ -65,7 +73,7 @@ out_parsed = args.out_parsed
 out_filtered = args.out_parsed_filtered
 taxa_rank = args.tax_rank
 rank_value = args.rank_value
-
+out_fig = args.out_fig
 
 
 # In[3]:
@@ -152,3 +160,32 @@ df_test['ttest_adj_reject'] = multipletests(df_test['ttest_p'],alpha=0.05,method
 df_test.sort_values(by='ttest_adj_p').to_csv(out_file,index=False)
 
 df.loc[:,df.columns.isin(df_test.loc[(df_test['MannWhitney_adj_reject'] | df_test['ttest_adj_reject']),'feature'])].to_csv(out_filtered,index=False)
+
+def plot_sig(y_list ,df=df,df_test=df_test,x = 'Flag',output=out_fig):
+    # Tot and Cols are known
+
+    Tot = len(y_list)
+    Cols = 4
+
+    # Compute Rows required
+
+    Rows = Tot // Cols 
+    Rows += Tot % Cols
+
+    # Create a Position index
+
+    Position = range(1,Tot + 1)
+    # Create main figure
+    fig = plt.figure(1)
+    
+    for i,c in enumerate(y_list):
+        ax1 = fig.add_subplot(Rows,Cols,Position[i])
+        ax = sns.boxplot(data=df,x=x,y=c,ax=ax1)
+        ax.set_xticklabels(['Non-' + rank_value,rank_value])
+        ax.set_title("Adj_p_value = " + str(round(df_test.loc[df_test['feature'] == c,'ttest_adj_p'].item(),5)))
+    fig.text(0.5, 0.04, 'Species', ha='center')
+    fig.text(0.04, 0.5, 'Feature', va='center', rotation='vertical')
+    fig.tight_layout()
+    fig.savefig(output)
+
+plot_sig(y_list = df_test[(df_test['ttest_adj_reject'] == True)]['feature'].to_list())
